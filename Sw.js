c78fef719609f243726223359db21a0e4d603819
aryansinguzab-lab <aryansinguzab@gmail.com>
2026-02-16 20:54:26 +0300

    Update service worker cache version and assets

--- Sw.js
+++ Sw.js
@@ -1,23 +1,22 @@
-const CACHE_NAME = 'biz-registry-v6';
-// List every file you want to work offline
+const CACHE_NAME = 'biz-registry-v8';
 const ASSETS_TO_CACHE = [
-  './Index.html',    // Capitalized to match your file
+  './',
+  './index.html',
   './manifest.json',
-  './icon2.png'      // Your phone's icon image
+  './icon2.png',
+  // If you are using a local dexie file, add it here:
+  './dexie.js' 
 ];
 
-// Install: Save files to the browser's "Vault"
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      console.log('SW: Caching assets');
       return cache.addAll(ASSETS_TO_CACHE);
     })
   );
   self.skipWaiting();
 });
 
-// Activate: Delete old caches when you update the version
 self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((keys) => {
@@ -26,36 +25,14 @@ self.addEventListener('activate', (event) => {
       }));
     })
   );
-  self.clients.claim();
+  return self.clients.claim();
 });
 
-// Fetch: Intercept network requests
 self.addEventListener('fetch', (event) => {
+  // We ignore database requests and focus on app files
   event.respondWith(
     caches.match(event.request).then((response) => {
-      // Return cached file if found, otherwise go to internet
       return response || fetch(event.request);
     })
   );
 });
-evt.respondWith(
-    caches.open(cacheName).then(cache => {
-      return cache.match(evt.request).then(cachedRes => {
-        const fetchPromise = fetch(evt.request).then(networkRes => {
-          // Update the cache with the fresh version for NEXT time
-          if (networkRes.ok) {
-            cache.put(evt.request, networkRes.clone());
-          }
-          return networkRes;
-        }).catch(() => {
-            // If network fails and no cache, this prevents a crash
-            return cachedRes; 
-        });
-
-        // Return the cached version immediately (fast), or wait for network
-        return cachedRes || fetchPromise;
-      });
-    })
-  );
-});
-                 
